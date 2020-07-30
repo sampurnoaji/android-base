a14dcc7fd6ee91461248df45f2456e0a91da4d4a
Sampurno Aji <man.sanji23@gmail.com>
2020-07-30 21:34:51 +0700

    fix bottom sheet

--- app/src/main/java/id/petersam/base/presentation/MainActivity.kt
+++ app/src/main/java/id/petersam/base/presentation/MainActivity.kt
@@ -51,13 +51,11 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
     }
 
     fun showStatusBar() {
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE or
-                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
     }
 
     fun hideStatusBar() {
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
     }
 
--- app/src/main/java/id/petersam/base/presentation/home/HomeFragment.kt
+++ app/src/main/java/id/petersam/base/presentation/home/HomeFragment.kt
@@ -2,9 +2,7 @@ package id.petersam.base.presentation.home
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import id.petersam.base.R
@@ -23,10 +21,19 @@ class HomeFragment : Fragment() {
                 requireActivity().finish()
             }
         })
-
+        setupToolbar()
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    private fun setupToolbar() {
+        (activity as MainActivity).showToolbar()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_top, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
     override fun onAttach(context: Context) {
         super.onAttach(context)
         (activity as MainActivity).showStatusBar()
